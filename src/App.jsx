66666d7a3e8334cb868d9c436f26f66366375ebf
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-02-27 00:20:26 +0530

    Completely converted a nextjs Aceternity-UI component into vite+React component in js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import { BackgroundBeamsWithCollision } from './components/ui/BBC';
+
+function App() {
+  return (
+    <main>
+      <BackgroundBeamsWithCollision />
+    </main>
+  );
+}
+
+export default App;
